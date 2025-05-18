52157e714057cee31089c9b20b269328dbd508ae
chengwei.fu <fuchengwei0127@gmail.com>
2025-05-18 17:48:18 +0800

    feat(entity): add auditing fields and annotations

--- src/main/java/com/github/springbootquickstart/entity/Auditing.java
+++ src/main/java/com/github/springbootquickstart/entity/Auditing.java
@@ -1,4 +1,29 @@
 package com.github.springbootquickstart.entity;
 
-public class Auditing {
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public abstract class Auditing {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
